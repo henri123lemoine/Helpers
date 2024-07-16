7904129be0f3bf4bf6feff4ed2281c8d081541f9
Henri Lemoine <henri@zeroth.sh>
2024-07-16 03:07:07 -0400

    Updated settings

--- src/settings.py
+++ src/settings.py
@@ -7,10 +7,15 @@ load_dotenv()
 
 
 # General
-DATE = datetime.now().strftime("%Y-%m-%d")
+DATE = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 PROJECT_PATH = PROJECT_DIR = Path(__file__).resolve().parent.parent
 
 # Data
 DATA_PATH = PROJECT_PATH / "data"
 CACHE_PATH = DATA_PATH / ".cache"
 PLOTS_PATH = DATA_PATH / "plots"
+HISTORY_PATH = DATA_PATH / "history"
+PYTHON_NOTEBOOKS_PATH = DATA_PATH / "python_notebooks"
+
+for path in [CACHE_PATH, PLOTS_PATH, HISTORY_PATH, PYTHON_NOTEBOOKS_PATH]:
+    path.mkdir(parents=True, exist_ok=True)
